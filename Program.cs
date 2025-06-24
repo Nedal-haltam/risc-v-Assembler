ebcd566c3c0b2183d98ba610757ed1a0d0d558c0
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-06-24 22:48:43 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace risc_v_Assembler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
