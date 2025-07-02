375afbeec36d7839e49b335a607b2f5e2805e012
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-07-02 14:39:25 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -45,45 +45,42 @@ namespace risc_v_Assembler
         }
         static void HandleCommand(string[] args)
         {
-            Shartilities.ShiftArgs(ref args, out string source_filepath);
-            Shartilities.ShiftArgs(ref args, out string MC_filepath);
-            Shartilities.ShiftArgs(ref args, out string DM_filepath);
-            Shartilities.ShiftArgs(ref args, out string IM_INIT_filepath);
-            Shartilities.ShiftArgs(ref args, out string DM_INIT_filepath);
-            Shartilities.ShiftArgs(ref args, out string IM_MIF_filepath);
-            Shartilities.ShiftArgs(ref args, out string DM_MIF_filepath);
+            if (!Shartilities.ShiftArgs(ref args, out string source_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"source filepath is not provided", 1);
+            if (!Shartilities.ShiftArgs(ref args, out string MC_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"MC filepath is not provided", 1);
+            if (!Shartilities.ShiftArgs(ref args, out string DM_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"DM filepath is not provided", 1);
+            if (!Shartilities.ShiftArgs(ref args, out string IM_INIT_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"IM INIT filepath is not provided", 1);
+            if (!Shartilities.ShiftArgs(ref args, out string DM_INIT_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"DM INIT filepath is not provided", 1);
+            if (!Shartilities.ShiftArgs(ref args, out string IM_MIF_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"IM MIF filepath is not provided", 1);
+            if (!Shartilities.ShiftArgs(ref args, out string DM_MIF_filepath))
+                Shartilities.Log(Shartilities.LogType.ERROR, $"DM MIF filepath is not provided", 1);
 
             List<string> DM_INIT = [], DM = [];
-            if (source_filepath != null)
-            {
-                List<string> src = File.ReadAllLines(source_filepath).ToList();
-                LibUtils.LibUtils.clean_comments(ref src);
-                (List<string> data_dir, List<string> text_dir) = LibUtils.LibUtils.Get_directives(src);
-                curr_data_dir = data_dir;
-                curr_text_dir = text_dir;
-                (List<string> DM_INIT1, List<string> DM1, List<KeyValuePair<string, int>> addresses) = LibUtils.LibUtils.assemble_data_dir(curr_data_dir);
-                DM_INIT = DM_INIT1;
-                DM = DM1;
-                Assemble(addresses);
-            }
+            List<string> src = [.. File.ReadAllLines(source_filepath)];
+            LibUtils.LibUtils.clean_comments(ref src);
+            (List<string> data_dir, List<string> text_dir) = LibUtils.LibUtils.Get_directives(src);
+            curr_data_dir = data_dir;
+            curr_text_dir = text_dir;
+            (List<string> DM_INIT1, List<string> DM1, List<KeyValuePair<string, int>> addresses) = LibUtils.LibUtils.assemble_data_dir(curr_data_dir);
+            DM_INIT = DM_INIT1;
+            DM = DM1;
+            Assemble(addresses);
 
-            if (MC_filepath != null)
-                File.WriteAllLines(MC_filepath, m_prog.mc);
-            if (DM_filepath != null)
-                File.WriteAllLines(DM_filepath, DM);
+            File.WriteAllLines(MC_filepath, m_prog.mc);
+            File.WriteAllLines(DM_filepath, DM);
 
-            if (IM_INIT_filepath != null)
-            {
-                List<string> IM_INIT = LibUtils.LibUtils.get_IM_INIT(m_prog.mc, curr_insts);
-                File.WriteAllLines(IM_INIT_filepath, IM_INIT);
-            }
-            if (DM_INIT_filepath != null)
-                File.WriteAllLines(DM_INIT_filepath, DM_INIT);
+            List<string> IM_INIT = LibUtils.LibUtils.get_IM_INIT(m_prog.mc, curr_insts);
+            File.WriteAllLines(IM_INIT_filepath, IM_INIT);
 
-            if (IM_MIF_filepath != null)
-                File.WriteAllText(IM_MIF_filepath, LibUtils.LibUtils.GetIMMIF(m_prog.mc, 32, 2048, 2).ToString());
-            if (DM_MIF_filepath != null)
-                File.WriteAllText(DM_MIF_filepath, LibUtils.LibUtils.GetDMMIF(DM, 32, 4096, 10).ToString());
+            File.WriteAllLines(DM_INIT_filepath, DM_INIT);
+
+            File.WriteAllText(IM_MIF_filepath, LibUtils.LibUtils.GetIMMIF(m_prog.mc, 32, 2048, 2).ToString());
+            File.WriteAllText(DM_MIF_filepath, LibUtils.LibUtils.GetDMMIF(DM, 32, 4096, 10).ToString());
         }
         static void Main(string[] args)
         {
